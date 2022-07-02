438e573a74a82f1f9f8b092ad8c904789dd38ec5
kremelkeyz <keyernmwihandi16@gmail.com>
2022-07-01 18:48:17 -0700

    positive

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,13 +7,13 @@
 int main(void)
 {
 int i, j;
-for (i = 0; i < 9; i++)
+for (i = 0; i < 10; i++)
 {
-for (j = 0; j < 9; j++)
+for (j = 0; j < 10; j++)
 {
 putchar(i + '0');
 putchar(j + '0');
-if ((i == 8) && (j == 9))
+if ((i == 9) && (j == 9))
 ;
 else
 {
