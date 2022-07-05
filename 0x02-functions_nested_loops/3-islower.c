2dd5780e0a04827382d9b8779bb4fdffb1aedf74
kremelkeyz <keyernmwihandi16@gmail.com>
2022-07-05 08:33:34 -0700

    lower case confirmation

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _islower - Entry point
+ * @c: displays lowercase letters
+ * Return: 0
+ */
+
+int _islower(int c)
+{
+if (c >= 97)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
