3c629d0ec15c69efa320ea348990ff14354e683b
kremelkeyz <keyernmwihandi16@gmail.com>
2022-07-05 08:39:04 -0700

    isalapha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _isalpha - Entry point
+* @c: displays letters in the alphabet
+* Return: 0
+*/
+
+int _isalpha(int c)
+{
+if (c >= 65)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
