cf69acb4460a9a71b3e2338e71c2deaf6607a151
kremelkeyz <keyernmwihandi16@gmail.com>
2022-07-07 15:18:31 -0700

    More loops

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,22 +8,23 @@ include "main.h"
 
 void print_triangle(int size)
 {
-int i;
-int j;
-int k;
+int i = 0, j, n = size - 1;
 
-if (size <= 0)
-_putchar('\n');
-for (i = 1; i <= size; i++)
+if (size > 0)
+{
+for (; i < size; i++)
 {
 for (j = 0; j < size; j++)
 {
+if (j < n)
 _putchar(' ');
-}
-for (k = 1; k <= i; k++)
-{
+else
 _putchar('#');
 }
+n--;
 _putchar('\n');
 }
+}
+else
+_putchar('\n');
 }
