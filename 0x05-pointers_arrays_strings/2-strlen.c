9a178d79bc95a03dd8acad47c239b85b4f1ff8f0
kremelkeyz <keyernmwihandi16@gmail.com>
2022-07-11 16:13:40 -0700

    pointers and arrays

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+
+
+/**
+ *_strlen - calculate the length of a string
+ *@s: string
+ *Return: length of input string
+ */
+
+int _strlen(char *s);
+{
+int index;
+
+for (index = 0; s[index] != '\0' ; index++)
+return (index);
+}
