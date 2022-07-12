96d36a6c9b545fa7d34016c9f958de7f1a78332a
kremelkeyz <keyernmwihandi16@gmail.com>
2022-07-11 18:05:26 -0700

    pointers and arrays

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * char *_strcpy - copying string pointers
+ * @dest: copy tp
+ * @src: copy from
+ * Return: string
+ */
+char *_strcpy(char *dest, char *src)
+{
+int x = 0;
+int l = 0;
+
+while (*(src + l) != '\0')
+{
+l++;
+}
+for ( ; x < l ; x++)
+{
+dest[x] = src[x];
+}
+dest[l] = '\0';
+return (dest);
+}
