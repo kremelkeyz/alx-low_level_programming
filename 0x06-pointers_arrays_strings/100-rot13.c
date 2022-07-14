b8a28685cd68b2deb51cb08b13eeaea46d009fcc
kremelkeyz <keyernmwihandi16@gmail.com>
2022-07-14 14:57:29 -0700

    still more pointers

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * rot13 - Entry point
+ * ONE if, TWO loops only...
+ * @s: input
+ * Return: decrypted string
+ */
+
+char *rot13(char *s))
+{
+char *c;
+char shift;
+
+for (c = s; *c; ++c)
+{
+shift = 'A' + (*c & 32);
+if (('a' <= *c && *c <= 'z') || ('A' <= *c && *c <= 'Z'))
+*c = (*c - shift + 13) % 26 + shift;
+}
+return (s);
+}
