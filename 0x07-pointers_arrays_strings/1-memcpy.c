df75d6f42c2bce16dd373305cd0a144bfa5ab501
kremelkeyz <keyernmwihandi16@gmail.com>
2022-07-18 07:21:59 -0700

    even more pointers

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,9 +8,8 @@
  *Return: string copied from source
 */
 
-char *_memcpy(char *dest, char *src, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
-
 unsigned int a;
 for (a = 0; a < n; a++)
 dest[a] = src[a];
