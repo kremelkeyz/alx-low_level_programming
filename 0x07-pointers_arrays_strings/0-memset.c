bfcdf449968b3e6e3a05ae4e810e09a2df8e5ea2
kremelkeyz <keyernmwihandi16@gmail.com>
2022-07-17 17:24:39 -0700

    memset successfuly done

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _memset - this is a memory set function
+* @s: string
+* @b:  a character
+* @n: an integer
+* Return: a string
+*/
+
+char *_memset(char *s, char b, unsigned int n);
+{
+unsigned int k;
+
+for (k = 0; k < n; a++)
+s[k] = b;
+
+return (s);
+}
