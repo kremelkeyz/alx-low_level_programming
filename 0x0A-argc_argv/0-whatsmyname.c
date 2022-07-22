dd3bc8cb394253954022b541ba3331a8512a8e0d
-v <keyernmwihandi16@gmail.com>
2022-07-23 00:19:14 +0300

    argc argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-#include "main.h"
 /**
  * main -> this is a function to print its name
  * @argc: argc parameter
  * @argv: an array of command listed
  * Return: 0 for success
  */
-int main (int argc _attribute_((unused)), char *argv[])
+int main(int argc_attribute_((unused)), char *argv[])
 {
 printf("%s", *argv);
 return (0);
